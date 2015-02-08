36c51924f0c9ed81d6afbc6afc40dc2438deb2d6
greenray <greenray@shark>
2015-02-08 07:05:07 +0400

    Edited Signed-off-by:greenray <greenray@shark>

--- phpapi.php
+++ phpapi.php
@@ -92,7 +92,7 @@ require 'classes/tag.php';
 # Include phpapi class
 set_include_path(get_include_path().PATH_SEPARATOR.dirname(__FILE__));
 
-require 'classes'.DS.'phpapi.php';
+require 'classes'.DS.'phpAPI.php';
 
 # Get name of config file to use
 if (!isset($argv[1])) {
